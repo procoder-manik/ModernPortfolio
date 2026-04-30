fcd4d85c1d618a61247c6867da96681546fc315c
procoder-manik <manikhossainfarhan99@gmail.com>
2026-04-30 12:10:01 +0600

    new update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ModernPortfolio/' : '/',
+  base: process.env.DEPLOY === 'true' ? '/ModernPortfolio/' : '/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
